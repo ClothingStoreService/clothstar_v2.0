a4b978ce072c1eff5357515cefd3ff4448ea0a91
Ogu1208 <kdasunb@naver.com>
2024-05-26 14:51:36 +0900

    feat: BaseTimeEntity + @EnableJpaAuditing 설정
    - BaseTimeEntity 구현
    - JPAConfig 파일에 @EnableJpaAuditing 설정

---
+++ src/main/java/org/store/clothstar/common/JPAConfig.java
@@ -0,0 +1,10 @@
+package org.store.clothstar.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JPAConfig {
+
+}
