8663be4692b99ea521f1a5097aa043abf04927e6
hyunsu <rkdgustn77@naver.com>
2024-05-31 02:21:38 +0900

    refactor: 에러수정

--- src/main/java/org/store/clothstar/member/domain/Address.java
+++ src/main/java/org/store/clothstar/member/domain/Address.java
@@ -1,11 +1,15 @@
 package org.store.clothstar.member.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.store.clothstar.member.entity.AddressEntity;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private Long addressId;
     private Long memberId;
--- src/main/java/org/store/clothstar/member/service/AddressBasicServiceImpl.java
+++ src/main/java/org/store/clothstar/member/service/AddressBasicServiceImpl.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Service;
 import org.store.clothstar.member.domain.Address;
 import org.store.clothstar.member.dto.request.CreateAddressRequest;
 import org.store.clothstar.member.dto.response.AddressResponse;
-import org.store.clothstar.member.repository.AddressMybatisRepository;
 import org.store.clothstar.member.repository.AddressRepository;
-import org.store.clothstar.member.repository.SellerRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,7 +17,7 @@ public class AddressBasicServiceImpl implements AddressBasicService {
     private final AddressRepository addressRepository;
 
     public AddressBasicServiceImpl(
-            @Qualifier("addressJpaRepository") AddressRepository addressRepository) {
+            @Qualifier("addressJpaRepositoryAdapter") AddressRepository addressRepository) {
         //@Qualifier("addressMybatisRepository") AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
     }
@@ -39,7 +37,7 @@ public class AddressBasicServiceImpl implements AddressBasicService {
         if (result == 0) {
             throw new IllegalArgumentException("회원 배송지 주소가 저장되지 않았습니다.");
         }
-        
+
         return address.getAddressId();
     }
 }
