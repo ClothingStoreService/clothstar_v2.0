0d5fd5d8e49f7401c28ab0bf9dbb8d86794ced98
subin <axhtl@gachon.ac.kr>
2024-06-01 17:42:51 +0900

    refactor: OrderDetail 연관관계 설정(Product, ProductLine 제외)

--- src/main/java/org/store/clothstar/orderDetail/entity/OrderDetailEntity.java
+++ src/main/java/org/store/clothstar/orderDetail/entity/OrderDetailEntity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.store.clothstar.order.entity.OrderEntity;
 
 @Getter
 @AllArgsConstructor
@@ -16,15 +17,6 @@ public class OrderDetailEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderDetailId;
-
-    @Column(name="order_id")
-    private Long orderId;
-
-    @Column(name="product_line_id")
-    private Long productLineId;
-
-    @Column(name="product_id")
-    private Long productId;
 
     private int quantity;
 
@@ -41,15 +33,19 @@ public class OrderDetailEntity {
     private Long stock; // 옵션 상품 재고
     @Column(name="option_name")
     private String optionName;
-//    @Column(name="order_id")
-//    private int extraCharge;
 
     // 판매자 정보
     @Column(name="brand_name")
     private String brandName;
 
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private OrderEntity order;
 
-//    @Enumerated(EnumType.STRING)
+    @Column(name="product_line_id")
+    private Long productLineId;
 
+    @Column(name="product_id")
+    private Long productId;
 }
 
--- src/main/java/org/store/clothstar/orderDetail/repository/JpaOrderDetailRepositoryAdapter.java
+++ src/main/java/org/store/clothstar/orderDetail/repository/JpaOrderDetailRepositoryAdapter.java
@@ -1,15 +1,22 @@
 package org.store.clothstar.orderDetail.repository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+import org.store.clothstar.order.entity.OrderEntity;
+import org.store.clothstar.order.repository.order.JpaOrderRepository;
 import org.store.clothstar.orderDetail.domain.OrderDetail;
 import org.store.clothstar.orderDetail.entity.OrderDetailEntity;
 
 @Repository
 public class JpaOrderDetailRepositoryAdapter implements UpperOrderDetailRepository {
     private final JpaOrderDetailRepository jpaOrderDetailRepository;
+    JpaOrderRepository jpaOrderRepository;
 
-    public JpaOrderDetailRepositoryAdapter(JpaOrderDetailRepository jpaOrderDetailRepository){
+    public JpaOrderDetailRepositoryAdapter(JpaOrderDetailRepository jpaOrderDetailRepository,
+                                           JpaOrderRepository jpaOrderRepository){
         this.jpaOrderDetailRepository = jpaOrderDetailRepository;
+        this.jpaOrderRepository = jpaOrderRepository;
     }
 
     @Override
@@ -19,9 +26,12 @@ public class JpaOrderDetailRepositoryAdapter implements UpperOrderDetailReposito
     }
 
     private OrderDetailEntity convertToOrderEntity(OrderDetail orderDetail) {
+        OrderEntity order = jpaOrderRepository.findById(orderDetail.getOrderId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다."));
+
         return OrderDetailEntity.builder()
                 .orderDetailId(orderDetail.getOrderDetailId())
-                .orderId(orderDetail.getOrderId())
+                .order(order)
                 .productLineId(orderDetail.getProductLineId())
                 .productId(orderDetail.getProductId())
                 .quantity(orderDetail.getQuantity())
