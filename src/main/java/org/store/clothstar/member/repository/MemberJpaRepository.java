56d11803757d8ab22d1f65ceea579262dc1ec993
hyunsu <rkdgustn77@naver.com>
2024-06-25 01:17:47 +0900

    feat: member offset 페이징 적용

--- src/main/java/org/store/clothstar/member/repository/MemberJpaRepository.java
+++ src/main/java/org/store/clothstar/member/repository/MemberJpaRepository.java
@@ -1,10 +1,17 @@
 package org.store.clothstar.member.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.store.clothstar.member.entity.MemberEntity;
 
 import java.util.Optional;
 
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long>, MemberRepository {
     Optional<MemberEntity> findByEmail(String email);
+
+    @Query(value = "select m from member m",
+            countQuery = "select count(m) from member m")
+    Page<MemberEntity> findAllOffsetPaging(Pageable pageable);
 }
