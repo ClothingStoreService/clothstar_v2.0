d1589e01272fe7167c528f2b532fbfe737dfddc3
hyunsu <rkdgustn77@naver.com>
2024-07-06 03:57:55 +0900

    test: test code 수정

--- src/test/java/org/store/clothstar/ApplicationTests.java
+++ src/test/java/org/store/clothstar/ApplicationTests.java
@@ -1,6 +1,5 @@
 package org.store.clothstar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,8 +8,5 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertThat(true).isFalse();
-        Assertions.assertThat(true).isFalse();
     }
-
 }
