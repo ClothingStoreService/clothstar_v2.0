f53009170ad3fcb71e9fd2a863cf4b12ca17e371
Ogu1208 <kdasunb@naver.com>
2024-07-12 22:43:38 +0900

    test: 삭제된 기능 테스트에서 제거

--- src/test/java/org/store/clothstar/productLine/service/ProductLineServiceTest.java
+++ src/test/java/org/store/clothstar/productLine/service/ProductLineServiceTest.java
@@ -1,5 +1,6 @@
 package org.store.clothstar.productLine.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -89,38 +90,6 @@ class ProductLineServiceTest {
         assertThat(response.get(0).getPrice()).isEqualTo(13000);
         assertThat(response.get(0).getProductLineStatus()).isEqualTo(ProductLineStatus.COMING_SOON);
         assertThat(response.get(0).getBrandName()).isEqualTo("브랜드1");
-    }
-
-    @DisplayName("product_line_id로 상품 단건 조회에 성공한다.")
-    @Test
-    public void givenProductLineId_whenGetProductLineById_thenProductLineReturned() {
-        // given
-        Long productLineId = 1L;
-        ProductLineEntity productLine = mock(ProductLineEntity.class);
-        Seller seller = mock(Seller.class);
-
-        when(productLine.getProductLineId()).thenReturn(productLineId);
-        when(productLine.getSeller()).thenReturn(seller);
-        when(seller.getBrandName()).thenReturn("내셔널지오그래픽키즈 제주점");
-        when(productLine.getName()).thenReturn("내셔널지오그래픽 곰돌이 후드티");
-        when(productLine.getContent()).thenReturn("귀여운 곰돌이가 그려진 후드티에요!");
-        when(productLine.getPrice()).thenReturn(69000);
-        when(productLine.getStatus()).thenReturn(ProductLineStatus.ON_SALE);
-
-        given(productLineRepository.findById(productLineId)).willReturn(Optional.of(productLine));
-
-        // when
-        Optional<ProductLineResponse> response = productLineService.getProductLine(productLineId);
-
-        // then
-        assertThat(response).isPresent();
-        response.ifPresent(productLineResponse -> {
-            assertThat(productLineResponse.getBrandName()).isEqualTo("내셔널지오그래픽키즈 제주점");
-            assertThat(productLineResponse.getName()).isEqualTo("내셔널지오그래픽 곰돌이 후드티");
-            assertThat(productLineResponse.getContent()).isEqualTo("귀여운 곰돌이가 그려진 후드티에요!");
-            assertThat(productLineResponse.getPrice()).isEqualTo(69000);
-            assertThat(productLineResponse.getProductLineStatus()).isEqualTo(ProductLineStatus.ON_SALE);
-        });
     }
 
     @DisplayName("상품 id와 상품과 1:N 관계에 있는 상품 옵션 리스트를 조회한다.")
