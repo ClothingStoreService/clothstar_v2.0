4545e6e7190136b079f3c038c64666931e6e4d41
Ogu1208 <kdasunb6@gmail.com>
2024-07-20 01:54:50 +0900

    feat: SaleStatus, DisplayStatus 구현

---
+++ src/main/java/org/store/clothstar/productLine/domain/type/SaleStatus.java
@@ -0,0 +1,6 @@
+package org.store.clothstar.productLine.domain.type;
+
+public enum SaleStatus {
+    ON_SALE,
+    SOLD_OUT
+}
--- src/main/java/org/store/clothstar/productLine/dto/response/ItemResponse.java
+++ src/main/java/org/store/clothstar/productLine/dto/response/ItemResponse.java
@@ -1,5 +1,6 @@
 package org.store.clothstar.productLine.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.store.clothstar.productLine.domain.Item;
 import org.store.clothstar.productLine.domain.type.DeliveryType;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+@Builder
 public class ItemResponse {
 
     private Long id;
@@ -19,20 +21,18 @@ public class ItemResponse {
     private Integer remainStock;
     private List<ItemAttributeResponse> itemAttributes;
 
-    public ItemDTO(Item item) {
+    public ItemResponse(Item item) {
         this.id = item.getId();
         this.name = item.getName();
         this.price = item.getPrice();
-        this.finalPrice = item.getFinalPrice();
-        this.itemCode = item.getItemCode();
         this.deliveryType = item.getDeliveryType();
         this.itemAttributes = item.getItemAttributes().stream()
                 .map(ItemAttributeResponse::from)
                 .collect(Collectors.toList());
 
         // remainStock이 5개 이하일 경우에만 값을 설정
-        if (item.getRemainStock() != null && item.getRemainStock() <= 5) {
-            this.remainStock = item.getRemainStock();
+        if (item.getStock() != null && item.getStock() <= 5) {
+            this.remainStock = item.getStock();
         } else {
             this.remainStock = null;
         }
